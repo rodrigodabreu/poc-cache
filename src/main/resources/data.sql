89f3c45acced58427c23aee7c1e838bf203e7d35
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2023-04-18 08:23:16 -0300

    chore: create setup to use H2 as database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (first_name, last_name, email) VALUES ('Rodrigo', 'Abreu', 'rodrigo.abreu@outlook.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Diego', 'Abreu', 'diego.abreu@outlook.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Joao', 'Abreu', 'joao.abreu@outlook.com');
